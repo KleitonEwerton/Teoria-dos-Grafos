7fb49258a0e574c3ef80ab534f86a899eac62250
Daniel Muller <danielmrezende@yahoo.com.br>
2021-07-16 11:09:34 -0300

    Testando possibilidades

--- Headers/Graph.h
+++ Headers/Graph.h
@@ -49,7 +49,7 @@ public:
     void printGraph_Dot_Directed();
     void printGraph_Dot_Not_Directed();
     void directTransitiveClosing(int id);
-    void auxDirectTransitiveClosing(int id, bool *visited[]);
+    void auxDirectTransitiveClosing(int id, int *visited[]);
     void inDirectTransitiveClosing(int id);
     bool findEdge(int vet[], int k, int v);
     void deepSearch(int node);
