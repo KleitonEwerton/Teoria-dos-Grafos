90e326bedb943d5ec3aa269d1b29ca1965e92d73
guilherme-couto <guiguimcouto@gmail.com>
2021-07-20 16:35:49 -0300

    Sem duplicações

--- Headers/Graph.h
+++ Headers/Graph.h
@@ -1,84 +1,5 @@
-<<<<<<< HEAD
-/**************************************************************************************************
- * Implementation of the TAD Graph
-**************************************************************************************************/
 
-#ifndef GRAPH_H_INCLUDED
-#define GRAPH_H_INCLUDED
-#include "Node.h"
-#include <fstream>
-#include <vector>
-#include <stack>
-#include <list>
-
-using namespace std;
-
-class Graph
-{
-
-    //Atributes
-private:
-    int order;
-    int number_edges;
-    bool directed;
-    bool weighted_edge;
-    bool weighted_node;
-    Node *first_node;
-    Node *last_node;
-
-public:
-    //Constructor
-    Graph(int order, bool directed, bool weighted_edge, bool weighted_node);
-    //Destructor
-    ~Graph();
-    //Getters
-    int getOrder();
-    int getNumberEdges();
-    bool getDirected();
-    bool getWeightedEdge();
-    bool getWeightedNode();
-    Node *getFirstNode();
-    Node *getLastNode();
-    //Other methods
-    void insertNode(int id);
-    void insertEdge(int id, int target_id, float weight);
-    void removeNode(int id);
-    bool searchNode(int id);
-    Node *getNode(int id);
-    void printGraph();
-    void printGraph2();
-    void printGraph_Dot_Directed();
-    void printGraph_Dot_Not_Directed();
-    void directTransitiveClosing(int id);
-    void auxDirectTransitiveClosing(Node *node);
-    void indirectTransitiveClosing(int id);
-    void auxIndirectTransitiveClosing(Node *node);
-    bool findEdge(int vet[], int k, int v);
-    void deepSearch(int node);
-    void auxDeepSearch(Node *node, int vet[], int cont, vector<int> *ret);
-    void cleanVisited(); // Define todos os nós como não visitados.
-
-    //methods phase1
-    void topologicalSorting();
-    void breadthFirstSearch(ofstream &output_file);
-    Graph *getVertexInduced(int *listIdNodes);
-    Graph *agmKruskal();
-    Graph *agmPrim();
-    float floydMarshall(int idSource, int idTarget);
-    float dijkstra(int idSource, int idTarget);
-
-    //methods phase1
-    float greed();
-    float greedRandom();
-    float greedRactiveRandom();
-
-private:
-    //Auxiliar methods
-};
-
-#endif // GRAPH_H_INCLUDED
-=======
-/**************************************************************************************************
+/********************************************************************************************
  * Implementation of the TAD Graph
 **************************************************************************************************/
 
@@ -154,4 +75,3 @@ private:
 };
 
 #endif // GRAPH_H_INCLUDED
->>>>>>> 9eaf78d17ac558ea14627becffe20b45252229e1
--- Headers/Node.h
+++ Headers/Node.h
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-/**************************************************************************************************
- * Implementation of the TAD Node
-**************************************************************************************************/
-
-#ifndef NODE_H_INCLUDED
-#define NODE_H_INCLUDED
-#include "Edge.h" // Include of the Edge class
-
-using namespace std;
-
-// Definition of the Node class
-class Node{
-
-    // Attributes
-    private:
-        Edge* first_edge;
-        Edge* last_edge;
-        int id;
-        unsigned int in_degree;
-        unsigned int out_degree;
-        int degree;
-        float weight;
-        Node* next_node;
-        bool visited; // Atributo para definir se o nó foi visitado ou não.
-
-    public:
-        // Constructor
-        Node(int id);
-        // Destructor
-        ~Node();
-        // Getters
-        Edge* getFirstEdge();
-        Edge* getLastEdge();
-        int getId();
-        int getInDegree();
-        int getOutDegree();
-        int getDegree();
-        float getWeight();
-        Node* getNextNode();
-        bool getVisited(); // Retorna o atributo visited;
-        // Setters
-        void setNextNode(Node* node);
-        void setWeight(float weight);
-        void setVisited(bool visited); // Define se o nó foi visitado ou não.
-        // Other methods
-        bool searchEdge(int target_id);
-        void insertEdge(int target_id, float weight);
-        void removeAllEdges();
-        int removeEdge(int id, bool directed, Node* target_node);
-        void incrementOutDegree();
-        void decrementOutDegree();
-        void incrementInDegree();
-        void decrementInDegree();
-        Edge* hasEdgeBetween(int target_id);
-        // Auxiliar methods
-
-};
-
-#endif // NODE_H_INCLUDED
-=======
 /**************************************************************************************************
  * Implementation of the TAD Node
 **************************************************************************************************/
@@ -115,4 +54,3 @@ class Node{
 };
 
 #endif // NODE_H_INCLUDED
->>>>>>> 9eaf78d17ac558ea14627becffe20b45252229e1
